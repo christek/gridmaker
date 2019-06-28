81f289f66f888301802e2f1d21ad30b90715fd9f
Chris Tokar <christopher.tokar@gmail.com>
2019-06-28 16:02:46 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# gridmaker
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { styled } from "styletron-react";
+import _ from "lodash";
+
+import "./styles.css";
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+// https://gridbyexample.com/examples/example1/
+
+class GridContainerNew extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listItems: this.props.listItems,
+      colCount: 4
+    };
+    this.addItem = this.addItem.bind(this);
+    this.incrColumns = this.incrColumns.bind(this);
+  }
+
+  addItem() {
+    let foo = this.state.listItems;
+    foo.push(Math.floor(Math.random() * 10));
+    this.setState({ listItems: foo });
+  }
+  incrColumns() {
+    let foo = this.state.colCount + 1;
+    this.setState({ colCount: foo });
+  }
+
+  render() {
+    const RedAnchor = styled("div", {
+      color: "red"
+    });
+    let foo = "";
+    _.times(this.state.colCount, () => {
+      console.log("times");
+      foo += " 100px";
+    });
+    return (
+      <div>
+        <input type="button" value="add element" onClick={this.addItem} />
+        <input type="button" value="+" onClick={this.incrColumns} />
+        <RedAnchor className="container" style={{ gridTemplateColumns: foo }}>
+          {this.state.listItems.map(number => (
+            <div className="box">{number}</div>
+          ))}
+        </RedAnchor>
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <GridContainerNew listItems={numbers} />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
